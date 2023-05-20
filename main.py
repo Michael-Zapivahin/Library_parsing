2bafd9b9610a608522f9b65422a8c83ebe364f71
Michael-Zapivahin <zapivahin@ya.ru>
2023-05-20 11:40:25 +0300

    add new params

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import time
+import pickle
 
 import requests
 from bs4 import BeautifulSoup
@@ -31,7 +32,7 @@ def parse_book_page(response):
     }
 
 
-def download_books_genre(genre, books_dir, images_dir, comments_dir, start_page, end_page):
+def download_books_genre(genre, books_dir, images_dir, comments_dir, start_page, end_page, download_params):
     base_url = 'https://tululu.org'
     books_dir = os.path.join(books_dir, f'genre_{genre}')
     images_dir = os.path.join(images_dir, f'genre_{genre}')
@@ -44,10 +45,10 @@ def download_books_genre(genre, books_dir, images_dir, comments_dir, start_page,
         soup = parse_genre.get_soup(genre_page_url)
         for book_id in parse_genre.get_book_path(soup):
             book_number = ''.join(filter(lambda x: x.isdigit(), book_id))
-            download_book(base_url, book_number, books_dir, images_dir, comments_dir)
+            download_book(base_url, book_number, books_dir, images_dir, comments_dir, download_params)
 
 
-def download_book(base_url, book_id, books_dir, images_dir, comments_dir):
+def download_book(base_url, book_id, books_dir, images_dir, comments_dir, download_params):
     params = {'id': f'{book_id}'}
     try:
         book_response = requests.get(f'{base_url}/txt.php', params)
@@ -55,19 +56,44 @@ def download_book(base_url, book_id, books_dir, images_dir, comments_dir):
         url_processing.check_for_redirect(book_response)
         response = requests.get(f'{base_url}/b{book_id}')
         book_description = parse_book_page(response)
-        file_name = sanitize_filename(book_description['title'])
-        file_name = f'{os.path.join(books_dir, file_name)}.txt'
-        with open(file_name, 'w') as file:
-            file.write(book_response.text)
-        image_url = f'{base_url}/{book_description["image"]}'
-        expansion = url_processing.get_file_type(image_url)
-        file_name = f'{os.path.join(images_dir, book_description["title"])}.{expansion}'
-        url_processing.download_image(image_url, file_name)
+        if not download_params['skip_txt']:
+            file_name = sanitize_filename(book_description['title'])
+            if download_params['root_dir']:
+                books_dir = os.path.join(download_params['root_dir'], books_dir)
+            file_name = f'{os.path.join(books_dir, file_name)}.txt'
+            save_comments(comments_dir, book_description, download_params)
+            with open(file_name, 'w') as file:
+                file.write(book_response.text)
+        if not download_params['skip_img']:
+            image_url = f'{base_url}/{book_description["image"]}'
+            expansion = url_processing.get_file_type(image_url)
+            if download_params['root_dir']:
+                images_dir = os.path.join(download_params['root_dir'], images_dir)
+            file_name = f'{os.path.join(images_dir, book_description["title"])}.{expansion}'
+            url_processing.download_image(image_url, file_name)
     except requests.exceptions.HTTPError as net_error:
         print(f"book_id {book_id}: {net_error}")
     except requests.exceptions.ConnectionError as connect_error:
         print(f"book_id {book_id}: {connect_error}")
         time.sleep(10)
+
+
+def save_comments(comments_dir, description, download_params):
+    book_description = {
+        'Title: ': description['title'],
+        'Autor: ': description['author'],
+        'Genre: ': description['genre'],
+        'Comments: ': description['comments'],
+    }
+    if download_params['root_dir']:
+        file_name = download_params['root_dir']
+    elif download_params['json_path']:
+        file_name = download_params['json_path']
+    else:
+        file_name = comments_dir
+    file_name = f'{os.path.join(file_name, description["title"])}.txt'
+    with open(file_name, 'wb') as file:
+        pickle.dump(book_description, file)
 
 
 def main():
@@ -79,18 +105,37 @@ def main():
     comments_dir = os.getenv('COMMENTS')
     os.makedirs(comments_dir, exist_ok=True)
     parser = argparse.ArgumentParser(description='Script download books')
-    parser.add_argument('-s', '--start_id', help='first book id (default: 1)', type=int, default=1)
-    parser.add_argument('-e', '--end_id', help='last book id (default: 0)', type=int, default=0)
+    parser.add_argument('-s', '--start_page', help='first page id (default: 1)', type=int, default=1)
+    parser.add_argument('-e', '--end_page', help='last page id (default: 0)', type=int, default=0)
+    parser.add_argument('--skip_img', default=False, help='Turn off images download', type=bool)
+    parser.add_argument('--skip_txt', default=False, help='Turn off texts download', type=bool)
+    parser.add_argument('--root_dir', default='', help='Destination folder path', type=str)
+    parser.add_argument('--json_path', default='', help='JSON folder path', type=str)
     args = parser.parse_args()
-    start_id = args.start_id
-    end_id = args.end_id
+    start_id = args.start_page
+    end_id = args.end_page
+    download_params = {
+        'root_dir': args.root_dir,
+        'json_path': args.json_path,
+        'skip_img': args.skip_img,
+        'skip_txt': args.skip_txt,
+    }
+    base_url = 'https://tululu.org'
+    genre_id = 55
     if end_id == 0:
-        base_url = 'https://tululu.org'
-        genre_page_url = f"{base_url}/l{55}"
+        genre_page_url = f"{base_url}/l{genre_id}"
         soup = parse_genre.get_soup(genre_page_url)
         end_id = int(soup.select_one('body table p.center').contents[-1].text)
     if start_id < end_id:
-        download_books_genre(55, books_dir, images_dir, comments_dir, start_id, end_id)
+        download_books_genre(
+            genre_id,
+            books_dir,
+            images_dir,
+            comments_dir,
+            start_id,
+            end_id,
+            download_params
+        )
 
 
 if __name__ == '__main__':
