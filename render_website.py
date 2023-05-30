3b326c6b1494524f1b5fd52d66c0a5c1ff0903ca
Michael-Zapivahin <zapivahin@ya.ru>
2023-05-30 12:08:30 +0300

    Split books by two columns.

--- render_website.py
+++ render_website.py
@@ -1,5 +1,7 @@
 import json
 import os
+import glob
+from more_itertools import chunked
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import livereload
 
@@ -16,8 +18,6 @@ def main():
     # args = parser.parse_args()
     # json_path = args.json_path
 
-    json_path = 'comments/genre_55/book_59678.json'
-
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
@@ -26,21 +26,24 @@ def main():
     books_for_page = 10
     pages_count = 1
     page_id = 1
-    with open(f'{json_path}', 'r') as file:
-        book_descriptions = json.load(file)
-    image_path = book_descriptions['image'].split('/')
-    image_file = f'images/genre_55/book_{image_path[-1]}'
-    book_descriptions['image'] = image_file
-    # book_descriptions_per_page = list(chunked(book_descriptions, BOOKS_PER_PAGE))
-    # pages_count = math.ceil(len(book_descriptions)/BOOKS_PER_PAGE)
-    # for index, books in enumerate(book_descriptions_per_page, start=1):
-    #     books_for_page = list(chunked((books), BOOK_COLUMNS))
+    json_dir = 'comments/genre_55/'
+    os.makedirs('pages', exist_ok=True)
+
+    book_descriptions = []
+    for filepath in glob.glob(os.path.join(json_dir, '*.json')):
+        with open(filepath) as file:
+            book_description = json.load(file)
+        image_path = book_description['image'].split('/')
+        image_file = f'images/genre_55/book_{image_path[-1]}'
+        book_description['image'] = image_file
+        book_descriptions.append(book_description)
+    book_descriptions = list(chunked(book_descriptions, 2))
+    print(book_descriptions[0][0])
     rendered_page = template.render(
-        books=[book_descriptions],
+        books=book_descriptions,
         pages_count=pages_count,
         number_page=page_id
     )
-    os.makedirs('pages', exist_ok=True)
     with open(f'index{page_id}.html', 'w', encoding='utf8') as file:
         file.write(rendered_page)
 
